348e41be1b480ff0b0e9c81ac19e49faf930dbce
liuxinyang1984 <liuxinyang1984@gmail.com>
2023-11-07 17:54:48 +0800

    P436

--- vue3/cart/src/App.vue
+++ vue3/cart/src/App.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="approot">
         <es-header estitle="购物车" :fsize=24></es-header>
-        <h1>Root组件</h1>
-        <hr>
-        <div class="goods-list" v-for="goods in goodsList" :key="goods.goods_id">
-            <es-goods :goods="goods" :id="goods.goods_id"></es-goods>
+        <div class="goods-list" v-for="(goods,index) in goodsList" :key="goods.goods_id">
+            <es-goods :goods="goods" :id="goods.goods_id" @goodsStateChange="onGoodsStateChange"></es-goods>
         </div>
 
-        <es-footer :amount="0" :total="1" v-model:isfull="isfull" @fullChange="fullChange"></es-footer>
+        <es-footer :amount="amount" :total="total" v-model:isfull="isfull" @fullChange="fullChange"></es-footer>
     </div>
 </template>
 
@@ -25,7 +23,7 @@ export default {
     data(){
         return {
             goodsList:[],
-            isfull:true,
+            //isfull:false,
         }
     },
     methods:{
@@ -36,15 +34,56 @@ export default {
         },
         fullChange(val){
             console.log("父组件fullChange:"+val)
+            this.goodsList.forEach(goods=>{
+                goods.goods_state = val
+            })
+        },
+        onGoodsStateChange(e){
+            //console.log("ROOT.onGoodsStateChange:")
+            //console.log(e)
+            const goods = this.goodsList.find(item => item.goods_id === e.id)
+            if(goods){
+                goods.goods_state = e.state
+            }
+            //console.log(this.goodsList)
+
+
         }
     },
     created(){
         this.getGoodsList()
+    },
+    computed:{
+        amount(){
+            let amount= 0
+            this.goodsList
+                .filter(x => x.goods_state)
+                .forEach(x => {
+                amount += x.goods_price * x.goods_count
+            })
+            return amount
+        },
+        total(){
+            let total = 0
+            this.goodsList.filter(goods => goods.goods_state).forEach(goods => {
+                //console.log(goods)
+                total += goods.goods_count
+            })
+            return total
+        },
+        isfull(){
+            if(this.goodsList.filter(goods => !goods.goods_state).length > 0){
+                return false
+            }else{
+                return true
+            }
+        }
     }
 }
 </script>
 <style lang="less" scoped>
 .approot{
     padding-top: 48px;
+    padding-bottom: 56px;
 }
 </style>
--- vue3/cart/src/components/EsGoods.vue
+++ vue3/cart/src/components/EsGoods.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="goods-container">
         <div class="left">
-            <img :src="goods.goods_img" alt="" class="thumb">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" :id="'goods-'+goods.goods_id" :checked="goods.goods_state" @change="onInputChecked">
+                <label class="form-check-label" :for="'goods-'+goods.goods_id">
+                    <img :src="goods.goods_img" alt="" class="thumb">
+                </label>
+            </div>
         </div>
         <div class="right">
             <div class="top">{{goods.goods_name}}</div>
@@ -17,20 +22,40 @@
 export default {
     name:'EsGoods',
     props:['goods'],
+    emits:['goodsStateChange'],
+    methods:{
+        onInputChecked(e){
+            console.log('EsGoods:点击')
+            //console.log("赋值前:"+this.goods.goods_state)
+            //console.log('传参值:'+e.target.checked)
+            //const goodsTemp = this.goods
+            //goodsTemp.goods_state = e.target.checked
+            //console.log("赋值后:"+goodsTemp.goods_state)
+            this.$emit('goodsStateChange',{id:this.goods.goods_id,state:e.target.checked})
+        }
+    }
 }
 </script>
 
 <style lang="less" scoped>
 .goods-container{
+    border-top: 1px solid #efefef;
     display: flex;
     padding-bottom: 10px;
     .left{
         margin-right: 10px;
-        .thumb{
-            display: block;
-            width:100px;
-            height:100px;
-            background-color: #efefef;
+        .form-check{
+            .form-check-input{
+                margin-top: 43px;
+            }
+            .form-check-label{
+                .thumb{
+                    display: block;
+                    width:100px;
+                    height:100px;
+                    background-color: #efefef;
+                }
+            }
         }
     }
     .right{
